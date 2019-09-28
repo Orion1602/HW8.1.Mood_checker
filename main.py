19c4e53b4f67c57307148576fdcba4d9ecf47fe7
Stephanie Leeb <stephanieleeb@gmx.at>
2019-09-28 19:06:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+mood = input("Hej!! Hvernig hefur du dad??")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("Oh no. How can I help you?")
+
+elif mood == "excited":
+    print("How come?")
+
+elif mood == "relaxed":
+    print("Oooohh fine!")
+
+elif mood== "great":
+    print("Nice, that you are great again!!")
+
+else:
+    print("Really??")
